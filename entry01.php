5afd4f2b8bf47822dd549dea98e3d38fe7676aa2
Zhao Tan <maximtzh1000@gmail.com>
2015-04-08 22:27:12 -0500

    entry01

---
+++ entry01.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> My Webpage</title>
+	</head>
+	<body>
+		<p>Hello world!</p>
+		<p>I am Zhao!</p>
+		<?php
+		for($cnt=0;$cnt<10;$cnt++){
+			echo $cnt." ";
+		}
+		?>
+	</body>
+</html>
